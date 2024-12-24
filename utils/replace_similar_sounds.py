7d796489242edc2d7fbccaa3ea4b4f85c807dcba
farhansyedain <farhansyedain@gmail.com>
2024-12-24 20:59:02 +0530

    Move mapping to seperate file

--- utils/replace_similar_sounds.py
+++ utils/replace_similar_sounds.py
@@ -8,20 +8,7 @@ Ex: `ظ` with `ذ`
 import re
 import unicodedata
 
-similar_sounding_letters = {
-    "ع": {"representative": "ا", "remove_at_end": False},
-    "پھ": {"representative": "ف", "remove_at_end": False},
-    "ط": {"representative": "ت", "remove_at_end": False},
-    "ث" : {"representative": "س", "remove_at_end": False},
-    "ص": {"representative": "س", "remove_at_end": False},
-    "ڑ": {"representative": "ڈ", "remove_at_end": False},   
-    "ہ": {"representative": "ح", "remove_at_end": True},
-    "خ": {"representative": "کھ", "remove_at_end": False},
-    "غ": {"representative": "گ", "remove_at_end": False},
-    "ض": {"representative": "ذ", "remove_at_end": False},
-    "ظ": {"representative": "ذ", "remove_at_end": False},
-    "ذ": {"representative": "ز", "remove_at_end": False}
-}
+from mappings import similar_sounding_letters
 
 def replace_similar_sounds(text):
     text = unicodedata.normalize('NFC', text)
